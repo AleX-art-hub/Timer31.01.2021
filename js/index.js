d81b112a96115ce917e684f10d732b469bdcfc95
BodNS <bondarenokn@gmail.com>
2021-01-31 13:47:27 +0200

    add.numbers

---
+++ js/index.js
@@ -0,0 +1,7 @@
+'use strict'
+const valueElem=document.getElementById("timerValue");
+const startElem=document.getElementById("start");
+const stopElem=document.getElementById("stop");
+const resetElem=document.getElementById("reset");
+
+const date = new Date();
